b059b0854bd53c25cd0753f2357725330302fd81
udayempire <kuday0726@gmail.com>
2025-01-13 10:19:22 +0530

    added Dark and white mode

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-// import { ModeToggle } from "../ui/theme-icon"
+import { ModeToggle } from "../ui/theme-icon"
 
 export const Navbar = () => {
     const data = [
@@ -19,7 +19,7 @@ export const Navbar = () => {
     return <div className="flex justify-center md:justify-between items-center border-b border-zinc-700 p-4 mx-8">
         <div className="font-geist font-semibold">udayempire</div>
         <div className="flex items-center  gap-10">
-            {/* <ModeToggle /> */}
+            <ModeToggle />
             {data.map((link, index) => (
                 <li className="list-none" key={index}>
                     <Link className={`font-geist dark:hover:text-[#33E092] font-semibold hover:text-blue-800 transition ease-in-out  transis text-md`} href={link.href}>{link.name}</Link>
