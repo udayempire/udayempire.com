7deef32237074dd73ac827dd7c8562dde0d1797c
www.udayempire <147295044+udayempire@users.noreply.github.com>
2025-04-08 08:43:03 +0530

    updated last update

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -12,14 +12,14 @@ export const Footer = () => {
                 <div className="hidden md:block font-light">© Made with &#10084; by Uday</div>
                 <div className="hidden md:flex items-center gap-3">
                     <div className="bg-green-500 animate-pulse rounded-full w-2 h-2"></div>
-                    <p className="font-light md:text-md">Last Updated 06/04/25</p>
+                    <p className="font-light md:text-md">Last Updated 08/04/25</p>
                 </div>
                 {/* Mobile View */}
                 <div className="md:hidden flex gap-3 justify-between items-center">
                     <div className="font-light text-xs">© Made with &#10084; by Uday</div>
                     <div className="flex items-center gap-3">
                         <div className="bg-green-500 animate-pulse rounded-full w-2 h-2"></div>
-                        <p className="font-light text-xs">Last Updated 06/04/25</p>
+                        <p className="font-light text-xs">Last Updated 08/04/25</p>
                     </div>
                 </div>
             </div>
