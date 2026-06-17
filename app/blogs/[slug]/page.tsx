afdccbecfd833ef3e1bb4d5dfebefbacb59fbbe0
udayempire <kuday0726@gmail.com>
2026-06-17 21:19:55 +0530

    add youtube embed feature

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -114,6 +114,17 @@ const components = {
       {children}
     </td>
   ),
+  YouTubeEmbed: ({ videoId }: { videoId: string }) => (
+    <div className="my-6 w-full aspect-video rounded-xl overflow-hidden shadow-lg">
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="w-full h-full"
+      />
+    </div>
+  ),
 };
 
 interface PageProps {
